8511029663cf1675b71c0b8bf48727c68234124b
New Username <bscs22091@itu.edu.pk>
2024-09-22 19:31:28 +0500

    I have successfully updated 2nd page dynamically

--- script.js
+++ script.js
@@ -15,6 +15,37 @@ const mainPageData = {
     LogoSrc: "./mypic.jpg",
     SidePic: "./pic3.jpg"
 };
+
+// ABOUT PAGE 
+const AboutMySelf = {
+    title: "ABOUT US",
+    subtitle: "WHO AM I?",
+    description1: "I am Muhammad Hassan who is currently a student of bachelor's in computer science department here at ITU, Punjab Pakistan",
+    description2: "I've been here since 2022 and It's been an outstanding journey here. SO far, I have learned many skills including web-development as front end developer, coding skills, creativity, innovative ideas, and many more.",
+    features: [
+        {
+            icon: "./ideas_icon.png",
+            title: "Innovation",
+            description: "Focused on bringing creative solutions to challenging problems"
+        },
+        {
+            icon: "./web_icon.png",
+            title: "Web Design",
+            description: "Skilled in creating responsive, user-friendly web designs with a focus on clean layout and intuitive navigation."
+        },
+        {
+            icon: "./software_icon.png",
+            title: "Software",
+            description: "Experienced in developing efficient software solutions tailored to user needs"
+        },
+        {
+            icon: "./App_icon.png",
+            title: "Application",
+            description: "Focused on creating efficient, scalable applications for diverse platforms"
+        }
+    ]
+};
+
 window.onload = function()
 {
     // Update the sidebar menu
@@ -34,6 +65,21 @@ window.onload = function()
     // Update images
     document.querySelector(".logo img").src = mainPageData.LogoSrc;
     document.querySelector(".ppart2 img").src = mainPageData.SidePic;
+
+    // PAGE 2
+    // Populate the About section
+    document.querySelector("#About .leftspace:nth-of-type(1)").innerHTML = AboutMySelf.title;
+    document.querySelector("#About .bigfont").innerHTML = AboutMySelf.subtitle;
+    document.querySelector("#About p:nth-of-type(1)").innerHTML = AboutMySelf.description1;
+    document.querySelector("#About p:nth-of-type(2)").innerHTML = AboutMySelf.description2;
+
+    // Populate the features (Innovation, Web Design, etc.)
+    AboutMySelf.features.forEach((val, index) => {
+        document.querySelector(`.Box${index + 1} .iconsize`).src = val.icon;
+        document.querySelector(`.Box${index + 1} .bigfont`).innerHTML = val.title;
+        document.querySelector(`.Box${index + 1} p`).innerHTML = val.description;
+    });
+
 };
 
   
